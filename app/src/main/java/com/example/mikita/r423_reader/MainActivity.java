2858718616b792e26c41b9de260c600f14a49080
Gleb Linnik <glinnik@yandex-team.ru>
2019-04-27 08:29:55 +0300

    implement bottom navigation

--- app/src/main/java/com/example/mikita/r423_reader/MainActivity.java
+++ app/src/main/java/com/example/mikita/r423_reader/MainActivity.java
@@ -2,87 +2,29 @@ package com.example.mikita.r423_reader;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
 import static com.example.mikita.r423_reader.BookFragment.CONTENTS_ACTIVITY_RESULT;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        //navigationView.setCheckedItem(R.id.book);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        setupWithNavController(bottomNavigationView, navController);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        int id = menuItem.getItemId();
-
-        Fragment fragment = null;
-        switch (id){
-            case R.id.book:
-                fragment = BookFragment.getInstance();
-                break;
-            case R.id.books:
-                fragment = BooksFragment.getInstance();
-                break;
-            case R.id.gallery:
-                fragment = GalleryFragment.getInstance();
-                break;
-        }
-
-        if (fragment != null){
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content, fragment).commit();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode){
-            case CONTENTS_ACTIVITY_RESULT:
-                BookFragment.getInstance().onActivityResult(resultCode, data);
-                break;
-        }
-    }
 }
